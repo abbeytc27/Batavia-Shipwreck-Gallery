cadee3b2720e010a5c0be7db0b1fa78352966612
abbeytc27 <33941909@student.murdoch.edu.au>
2023-05-25 16:16:30 +0800

    Create exhibits.php

---
+++ exhibits.php
@@ -0,0 +1,49 @@
+<?php
+    /*
+    Template Name: Exhibits Page
+    */
+    ?>
+    <?php get_header();?>
+        <section class="container-fluid exhibitsp">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="images/consignments.png" class="exhibitimg" alt="consignments of the ship">
+                        <a href="consignment.html"><h4>Consignments</h4></a>
+                        <p class="exhibitsmp">The Batavia voyage was the maiden voyage of the Dutch East India Company (VOC) ship, 
+                        the Batavia, which sailed from Texel, Netherlands, on October 27, 1628.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="images/ins_out.png" class="exhibitimg" alt="the making of the ship">
+                        <a href="insandout.html"><h4>Ins and Outs</h4></a>
+                        <p class="exhibitsmp">The Batavia shipwreck occurred on June 4, 1629, when the Dutch East India Company (VOC) ship, 
+                            the Batavia, hit a reef near the Houtman Abrolhos islands off the coast of Western Australia.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="images/historys.png" class="exhibitimg" alt="the history of the ship">
+                        <a href="history.html"><h4>The History</h4></a>
+                        <p class="exhibitsmp">After the events of the Batavia shipwreck, 
+                            the remaining survivors were eventually rescued and taken to Batavia (modern-day Jakarta).</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="images/ship.png" class="exhibitimg" alt="the ship">
+                        <a href="exhibits.html"><h4>The Boat</h4></a>
+                        <p class="exhibitsmp">The ship was captained by Francisco Pelsaert and was carrying a cargo of silver, gold, and valuable trade goods, 
+                            as well as 341 passengers, including soldiers and colonists destined for the Dutch East Indies.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="images/wreck.png" class="exhibitimg" alt="the survivors">
+                        <a href="exhibits.html"><h4>The Survivors</h4></a>
+                        <p class="exhibitsmp">The ship was on its maiden voyage, carrying a cargo of silver, gold, and valuable trade goods, 
+                            as well as 341 passengers, including soldiers and colonists destined for the Dutch East Indies.
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="images/body.png" class="exhibitimg" alt="the wrecking of the ship">
+                        <a href="exhibits.html"><h4>The Wreck</h4></a>
+                        <p class="exhibitsmp">The wreck site remained largely untouched for centuries until it was rediscovered in the 1960s by a group of divers.</p>
+                        </div>
+                </div>
+            </div>
+        </section>
+        <?php get_footer();?>
