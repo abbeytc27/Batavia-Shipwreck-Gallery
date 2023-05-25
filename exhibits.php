fdd8ccb46272828133ac115ae2b5c5df8cdc2e7a
abbeytc27 <33941909@student.murdoch.edu.au>
2023-05-25 16:36:11 +0800

    Update exhibits.php

--- exhibits.php
+++ exhibits.php
@@ -1,6 +1,6 @@
 <?php
     /*
-    Template Name: Exhibits Page
+    Template Name: Exhibits
     */
     ?>
     <?php get_header();?>
@@ -8,40 +8,34 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-4">
-                        <img src="images/consignments.png" class="exhibitimg" alt="consignments of the ship">
-                        <a href="consignment.html"><h4>Consignments</h4></a>
-                        <p class="exhibitsmp">The Batavia voyage was the maiden voyage of the Dutch East India Company (VOC) ship, 
-                        the Batavia, which sailed from Texel, Netherlands, on October 27, 1628.</p>
+                        <img src="<?php the_field('consignmentimage'); ?>" class="exhibitimg" alt="consignments of the ship">
+                        <a href="consignment.html"><h4><?php the_field('consignmenttitle'); ?></h4></a>
+                        <p class="exhibitsmp"><?php the_field('consignmentparagraph'); ?></p>
                     </div>
                     <div class="col-md-4">
-                        <img src="images/ins_out.png" class="exhibitimg" alt="the making of the ship">
-                        <a href="insandout.html"><h4>Ins and Outs</h4></a>
-                        <p class="exhibitsmp">The Batavia shipwreck occurred on June 4, 1629, when the Dutch East India Company (VOC) ship, 
-                            the Batavia, hit a reef near the Houtman Abrolhos islands off the coast of Western Australia.</p>
+                        <img src="<?php the_field('inoutimage'); ?>" class="exhibitimg" alt="the making of the ship">
+                        <a href="insandout.html"><h4><?php the_field('inouttitle'); ?></h4></a>
+                        <p class="exhibitsmp"><?php the_field('inoutparagraph'); ?></p>
                     </div>
                     <div class="col-md-4">
-                        <img src="images/historys.png" class="exhibitimg" alt="the history of the ship">
-                        <a href="history.html"><h4>The History</h4></a>
-                        <p class="exhibitsmp">After the events of the Batavia shipwreck, 
-                            the remaining survivors were eventually rescued and taken to Batavia (modern-day Jakarta).</p>
+                        <img src="<?php the_field('historyimage'); ?>" class="exhibitimg" alt="the history of the ship">
+                        <a href="history.html"><h4><?php the_field('historyimage'); ?></h4></a>
+                        <p class="exhibitsmp"><?php the_field('historytitle'); ?></p>
                     </div>
                     <div class="col-md-4">
-                        <img src="images/ship.png" class="exhibitimg" alt="the ship">
-                        <a href="exhibits.html"><h4>The Boat</h4></a>
-                        <p class="exhibitsmp">The ship was captained by Francisco Pelsaert and was carrying a cargo of silver, gold, and valuable trade goods, 
-                            as well as 341 passengers, including soldiers and colonists destined for the Dutch East Indies.</p>
+                        <img src="<?php the_field('boatimage'); ?>" class="exhibitimg" alt="the ship">
+                        <a href="exhibits.html"><h4><?php the_field('boattitle'); ?></h4></a>
+                        <p class="exhibitsmp"><?php the_field('boatparagraph'); ?></p>
                     </div>
                     <div class="col-md-4">
-                        <img src="images/wreck.png" class="exhibitimg" alt="the survivors">
-                        <a href="exhibits.html"><h4>The Survivors</h4></a>
-                        <p class="exhibitsmp">The ship was on its maiden voyage, carrying a cargo of silver, gold, and valuable trade goods, 
-                            as well as 341 passengers, including soldiers and colonists destined for the Dutch East Indies.
-                        </p>
+                        <img src="<?php the_field('survivorsimage'); ?>" class="exhibitimg" alt="the survivors">
+                        <a href="exhibits.html"><h4><?php the_field('survivorstitle'); ?></h4></a>
+                        <p class="exhibitsmp"><?php the_field('survivoursparagraph'); ?> </p>
                     </div>
                     <div class="col-md-4">
-                        <img src="images/body.png" class="exhibitimg" alt="the wrecking of the ship">
-                        <a href="exhibits.html"><h4>The Wreck</h4></a>
-                        <p class="exhibitsmp">The wreck site remained largely untouched for centuries until it was rediscovered in the 1960s by a group of divers.</p>
+                        <img src="<?php the_field('wreckimage'); ?>" class="exhibitimg" alt="the wrecking of the ship">
+                        <a href="exhibits.html"><h4><?php the_field('wrecktitle'); ?></h4></a>
+                        <p class="exhibitsmp"><?php the_field('wreckparagraph'); ?></p>
                         </div>
                 </div>
             </div>
