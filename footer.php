93bf12f08832eba4a5b7f5faa70e183019ea4f3e
abbeytc27 <33941909@student.murdoch.edu.au>
2023-06-01 09:42:05 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
             <img src="images/wam_logo.png" class="footwam" alt="wam logo"></div>
             <div class="col-md-6"><p class="footerp">The Western Australian Museum acknowledges and respects the Traditional Owners of their ancestral lands,
                  waters and skies.</p></div>
