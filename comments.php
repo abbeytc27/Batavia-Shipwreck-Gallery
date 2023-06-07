4fba12e9718987569e3d1c35b3bf358ebe7af9bd
abbeytc27 <33941909@student.murdoch.edu.au>
2023-06-07 16:32:28 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,14 +1,14 @@
-<?php // Do not delete these lines
+<?php 
 if ('comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) die ('Please do not load this page directly. Thanks!');
-/* Variable for alternating comment background */
+
 $oddcomment = 'alt';
 ?>
-<!-- You can start editing here. -->
+
 <?php if ($comments) : ?>
 <h3 id="comments"><?php comments_number('No Comments', '1 Comment', '% Comments');?></h3>
 <ol class="commentlist">
 <?php foreach ($comments as $comment) : ?>
-<li class="<?php /* Separate admin comments */ if ($comment->user_id) echo 'authcomment'; else echo $oddcomment; ?>" id="comment-<?php comment_ID() ?>">
+<li class="<?php if ($comment->user_id) echo 'authcomment'; else echo $oddcomment; ?>" id="comment-<?php comment_ID() ?>">
 <div class="commentmetadata">
 <span class="comment-author"><?php comment_author_link() ?></span>
 <span class="comment-date"><?php comment_date('F jS, Y') ?> at <?php comment_time() ?></span>
@@ -23,21 +23,21 @@ $oddcomment = 'alt';
 </div>
 <div style="clear:both;"></div>
 </li>
-<?php /* Changes every other comment to a different class */
+<?php 
 if ('alt' == $oddcomment) $oddcomment = '';
 else $oddcomment = 'alt';
 ?>
-<?php endforeach; /* end for each comment */ ?>
+<?php endforeach;  ?>
 </ol>
 
-<?php else : /* this is displayed if there are no comments so far */ ?>
+<?php else :  ?>
     
 <h3 id="comments">No comments</h3>
 <?php if ('open' == $post->comment_status) : ?>
-<!-- If comments are open, but there are no comments. -->
+
 <p class="nocomments">No comments yet.</p>
-<?php else : // comments are closed ?>
-<!-- If comments are closed. -->
+<?php else : ?>
+
 <p class="nocomments">Comments are closed.</p>
 <?php endif; ?>
 
@@ -62,5 +62,5 @@ else $oddcomment = 'alt';
 </p>
 <?php do_action('comment_form', $post->ID); ?>
 </form>
-<?php endif; // If registration required and not logged in ?>
-<?php endif; // if you delete this the sky will fall on your head ?>
+<?php endif; ?>
+<?php endif; ?>
