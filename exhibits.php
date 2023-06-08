c8a0d326675bdacf094dfc1597a4c6c617187bde
abbeytc27 <33941909@student.murdoch.edu.au>
2023-06-08 11:07:07 +0800

    Update exhibits.php

--- exhibits.php
+++ exhibits.php
@@ -3,41 +3,48 @@
     Template Name: Exhibits1
     */
     ?>
-    <?php get_header();?>
-        <section class="container-fluid exhibitsp">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <img src="<?php the_field('consignmentimage'); ?>" class="exhibitimg" alt="consignments of the ship">
-                        <a href="consignment.html"><h4><?php the_field('consignmenttitle'); ?></h4></a>
-                        <p class="exhibitsmp"><?php the_field('consignmentparagraph'); ?></p>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="<?php the_field('inoutimage'); ?>" class="exhibitimg" alt="the making of the ship">
-                        <a href="insandout.html"><h4><?php the_field('inouttitle'); ?></h4></a>
-                        <p class="exhibitsmp"><?php the_field('inoutparagraph'); ?></p>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="<?php the_field('historyimage'); ?>" class="exhibitimg" alt="the history of the ship">
-                        <a href="history.html"><h4><?php the_field('historytitle'); ?></h4></a>
-                        <p class="exhibitsmp"><?php the_field('historyparagraph'); ?></p>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="<?php the_field('boatimage'); ?>" class="exhibitimg" alt="the ship">
-                        <a href="exhibits.html"><h4><?php the_field('boattitle'); ?></h4></a>
-                        <p class="exhibitsmp"><?php the_field('boatparagraph'); ?></p>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="<?php the_field('survivorsimage'); ?>" class="exhibitimg" alt="the survivors">
-                        <a href="exhibits.html"><h4><?php the_field('survivorstitle'); ?></h4></a>
-                        <p class="exhibitsmp"><?php the_field('survivorsparagraph'); ?> </p>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="<?php the_field('wreckimage'); ?>" class="exhibitimg" alt="the wrecking of the ship">
-                        <a href="exhibits.html"><h4><?php the_field('wrecktitle'); ?></h4></a>
-                        <p class="exhibitsmp"><?php the_field('wreckparagraph'); ?></p>
-                        </div>
-                </div>
-            </div>
-        </section>
-        <?php get_footer();?>
+   <?php require_once('blog_header.php'); ?>
+    <div class="container-fluid maincontainer">
+    <div class="container">
+        <div class="row">
+            <section class="maintext">              
+            <?php 
+$args = array(
+  'category_name' =>  'BlogExhibits',
+ // 'posts_per_page'    =>   1
+);
+// the query
+$the_query = new WP_Query( $args ); ?>
+ 
+<?php if ( $the_query->have_posts() ) : ?>
+ 
+    <!-- pagination here -->
+ 
+    <!-- the loop -->
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+ <div class="thepost">
+<h2><?php the_title(); ?></h2> 
+    
+<?php
+                if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                    the_post_thumbnail('thumbnail');
+                } ?>
+<p class="about-text"><?php the_excerpt(); ?></p>
+    <a class="readmore" href="<?php the_permalink(); ?>"> CONTINUE READING</a>
+</div>    <?php endwhile; ?>
+    <!-- end of the loop -->
+ 
+    <!-- pagination here -->
+ 
+    <?php wp_reset_postdata(); ?>
+ 
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+</section>
+           
+     </div>
+    </div><!-- container  -->
+  </div>  <!-- container fluid -->
+<?php get_footer(); ?>
